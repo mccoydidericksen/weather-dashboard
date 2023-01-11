110e61a33749b1f669527d5a49b52f5163a1d12c
McCoy Didericksen <mccoy.did@gmail.com>
2023-01-10 22:38:19 -0700

    add weather api function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,22 @@
 var apiKey = "4a29cf0c04950e957247f8b23be385fd";
 
+function getWeather(lat, lon){
+    // format the weather api url
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
+    
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        // request was successful
+        if (response.ok) {
+        response.json().then(function(data) {
+            console.log(data);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+          }
+    });
+}
+
 function getCoordinates(city) {
   // format the weather api url
   var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
@@ -12,7 +29,7 @@ function getCoordinates(city) {
         console.log(data);
         var lat = data[0].lat;
         var lon = data[0].lon;
-
+        getWeather(lat, lon);
       });
     } else {
       alert("Error: " + response.statusText);

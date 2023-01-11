6e46fe848d570859b095d94a56a4fdba7cfedfba
McCoy Didericksen <mccoy.did@gmail.com>
2023-01-11 02:14:07 -0700

    add local storage and search history buttons

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,45 @@
 var apiKey = "4a29cf0c04950e957247f8b23be385fd";
+var searchButton = $(".btn");
 
-function displayCurrentWeather(temp, humidity, windSpeed, icon){
+function displaySearchHistory(){
+    var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+    var searchHistoryEl = document.querySelector("#search-history");
+    searchHistoryEl.innerHTML = "";
+    if(searchHistory){
+        var keys = Object.keys(searchHistory);
+        for(var i=0; i<keys.length; i++){
+            var searchHistoryItemEl = document.createElement("li");
+            searchHistoryItemEl.setAttribute("class", "list-group-item");
+            searchHistoryItemEl.setAttribute("id", keys[i]);
+            searchHistoryItemEl.textContent = keys[i];
+            searchHistoryItemEl.classList.add("btn");
+            searchHistoryItemEl.classList.add("btn-secondary");
+            searchHistoryItemEl.classList.add("btn-block");
+            searchHistoryItemEl.addEventListener("click", function(e){
+                var city = e.target.id;
+                var lat = searchHistory[city][0];
+                var lon = searchHistory[city][1];
+                getWeather(lat, lon, city);
+            })
+            searchHistoryEl.appendChild(searchHistoryItemEl);
+        }
+    }
+}
+
+function storeSearch(city, lat, lon){
+    var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+    if(searchHistory){
+        searchHistory[city] = [lat, lon];
+    }
+    else{
+        searchHistory = {};
+        searchHistory[city] = [lat, lon];
+    }
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+    displaySearchHistory();
+}
+
+function displayCurrentWeather(temp, humidity, windSpeed, icon, city){
     var currentIconEl = document.querySelector("#current-weather-icon");
     currentIconEl.setAttribute("src", "http://openweathermap.org/img/w/" + icon + ".png");
     var currentTempEl = document.querySelector("#current-temp");
@@ -12,7 +51,7 @@ function displayCurrentWeather(temp, humidity, windSpeed, icon){
     var currentDateEl = document.querySelector("#current-date");
     currentDateEl.textContent = dayjs().format("MM/DD/YYYY");
     var currentCityEl = document.querySelector("#city-name");
-    currentCityEl.textContent = "New York";
+    currentCityEl.textContent = city;
 }
 
 function displayForecast(forecastList){
@@ -56,7 +95,7 @@ function displayForecast(forecastList){
     }
 }
 
-function getWeather(lat, lon){
+function getWeather(lat, lon, city){
     // format the weather api url
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
     
@@ -69,8 +108,9 @@ function getWeather(lat, lon){
             var humidity = data.main.humidity;
             var windSpeed = data.wind.speed;
             var icon = data.weather[0].icon;
-            displayCurrentWeather(temp, humidity, windSpeed, icon);
+            displayCurrentWeather(temp, humidity, windSpeed, icon, city);
             getForecast(lat, lon);
+            storeSearch(city, lat, lon);
             });
         } else {
             alert("Error: " + response.statusText);
@@ -86,7 +126,6 @@ function getForecast(lat, lon){
         // request was successful
         if (response.ok) {
         response.json().then(function(data) {
-            console.log(data);
             var forecastList = data.list;
             displayForecast(forecastList);
             });
@@ -106,7 +145,7 @@ function getCoordinates(city) {
       response.json().then(function(data) {
         var lat = data.coord.lat;
         var lon = data.coord.lon;
-        getWeather(lat, lon);
+        getWeather(lat, lon, city);
       });
     } else {
       alert("Error: " + response.statusText);
@@ -114,4 +153,12 @@ function getCoordinates(city) {
   });
 }
 
-getCoordinates("New York");
+
+searchButton.on("click", function(event) {
+    event.preventDefault();
+    var city = $("#city").val();
+    getCoordinates(city);
+    $("#city").val("");
+ });
+
+ displaySearchHistory();

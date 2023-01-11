ac322cba65bf10790445d9c180fee4a380d1f659
McCoy Didericksen <mccoy.did@gmail.com>
2023-01-11 01:00:40 -0700

    add displayCurrentWeather function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,40 @@
 var apiKey = "4a29cf0c04950e957247f8b23be385fd";
 
+function displayCurrentWeather(temp, humidity, windSpeed){
+    var currentTempEl = document.querySelector("#current-temp");
+    currentTempEl.textContent = "Temperature: " + temp + "°F";
+    var currentHumidityEl = document.querySelector("#current-humidity");
+    currentHumidityEl.textContent = "Humidity: " + humidity + "%";
+    var currentWindSpeedEl = document.querySelector("#current-wind");
+    currentWindSpeedEl.textContent = "Wind Speed: " + windSpeed + " MPH";
+    var currentDateEl = document.querySelector("#current-date");
+    currentDateEl.textContent = dayjs().format("MM/DD/YYYY");
+    var currentCityEl = document.querySelector("#city-name");
+    currentCityEl.textContent = "New York";
+}
+
 function getWeather(lat, lon){
+    // format the weather api url
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
+    
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        // request was successful
+        if (response.ok) {
+        response.json().then(function(data) {
+            var temp = data.main.temp;
+            var humidity = data.main.humidity;
+            var windSpeed = data.wind.speed;
+            displayCurrentWeather(temp, humidity, windSpeed);
+            getForecast(lat, lon);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+          }
+    });
+}
+
+function getForecast(lat, lon){
     // format the weather api url
     var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
     
@@ -19,16 +53,15 @@ function getWeather(lat, lon){
 
 function getCoordinates(city) {
   // format the weather api url
+  
   var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-
   // make a request to the url
   fetch(apiUrl).then(function(response) {
     // request was successful
     if (response.ok) {
       response.json().then(function(data) {
-        console.log(data);
-        var lat = data[0].lat;
-        var lon = data[0].lon;
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
         getWeather(lat, lon);
       });
     } else {
@@ -37,4 +70,4 @@ function getCoordinates(city) {
   });
 }
 
-getWeather("New York");
+getCoordinates("New York");
